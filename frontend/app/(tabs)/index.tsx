d28edab5b0b3eb0ea3df7bc92b1c1baf21a50fa5
Daniel Campos <81268953+danielcperalba@users.noreply.github.com>
2025-12-14 12:04:23 -0300

    Refactor code structure for improved readability and maintainability

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 import { View } from '@/components/Themed';
 import { ListeningButton } from '@/components/listeningButton';
@@ -6,7 +6,14 @@ import { ListeningButton } from '@/components/listeningButton';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.title}>Tab One</Text> */}
+      <View style={styles.header}>
+        <View style={styles.userInfo}>
+          <Image style={styles.userImage} />
+          <Text style={styles.greeting}>Olá, </Text>
+          <Text style={styles.userName}>Daniel</Text>
+        </View>
+        <TouchableOpacity style={styles.cameraCircle} />
+      </View>
       <ListeningButton />
     </View>
   );
@@ -17,14 +24,50 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#141414',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#FFFFFF',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  header: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    padding: 20,
+    backgroundColor: 'transparent',
+    marginTop: 30,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  greeting: {
+    fontSize: 20,
+    color: '#ccccccff',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  cameraCircle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#333333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  userImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: '#333333',
+    marginRight: 10,
   },
 });
--- frontend/components/listeningButton/styles.ts
+++ frontend/components/listeningButton/styles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#1E40AF',
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
